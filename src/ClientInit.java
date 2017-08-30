a20123e56e8d7028f90c661b3868e7af253da6b1
Boongy <masonprochaska72@gmail.com>
2017-08-30 18:16:29 -0500

    Update ClientInit.java

--- src/ClientInit.java
+++ src/ClientInit.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class ClientInit {
 	public static void main(String[] args) {
 		Client base;
-		base = new Client("72.204.32.42");
+		base = new Client(ipAddress);
 		base.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		base.startRunning();
 	}
